15343a273cc3fd65b7edc999cbb3a2c303d45214
Kasumi Null <kasumi.null@yandex.com>
2023-10-27 06:26:04 +0800

    change bg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body style={{ backgroundImage: `url(${buildUrl("/bg-c.png")})` }}>
+      <body style={{ backgroundImage: `url(${buildUrl("/bg-landscape.png")})` }}>
         <Main />
         <NextScript />
       </body>
