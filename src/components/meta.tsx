d94df741e08bb2e480cce7d9f82175b81c9b5cd4
Kasumi Null <kasumi.null@yandex.com>
2023-10-29 09:13:53 +0800

    favicon

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -1,10 +1,10 @@
 import { buildUrl } from "@/utils/buildUrl";
 import Head from "next/head";
 export const Meta = () => {
-  const title = "ChatVRM";
+  const title = "Amica";
   const description =
-    "Webブラウザだけで3Dキャラクターとの会話を、マイクやテキスト入力、音声合成を用いて楽しめます。キャラクター（VRM）の変更や性格設定、音声調整もできます。";
-  const imageUrl = "https://pixiv.github.io/ChatVRM/ogp.png";
+    "Converse with AI powered 3D characters with voice synthesis and speech recognition.";
+  const imageUrl = "https://amica.arbius.ai/ogp.png";
   return (
     <Head>
       <title>{title}</title>
@@ -16,6 +16,16 @@ export const Meta = () => {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={imageUrl} />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="apple-mobile-web-app-title" content={title} />
+      <meta name="application-name" content={title} />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 };
