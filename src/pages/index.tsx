4e126d5a2926a4126d4a0bf7201009095c90edd1
Kasumi Null <kasumi.null@yandex.com>
2023-11-02 07:11:43 +0800

    merge menu into index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,23 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState, useRef } from "react";
 import { buildUrl } from "@/utils/buildUrl";
 import { M_PLUS_2, Montserrat } from "next/font/google";
+import { AssistantText } from "@/components/assistantText";
+import { IconButton } from "@/components/iconButton";
+import { ChatLog } from "@/components/chatLog";
 import VrmViewer from "@/components/vrmViewer";
+import { MessageInputContainer } from "@/components/messageInputContainer";
 import { ViewerContext } from "@/features/vrmViewer/viewerContext";
+import { Introduction } from "@/components/introduction";
+import { LoadingProgress } from "@/components/loadingProgress";
+import { Settings } from "@/components/settings";
+import { EmbeddedWebcam } from "@/components/embeddedWebcam";
 import {
   Message,
   textsToScreenplay,
   Screenplay,
 } from "@/features/messages/messages";
 import { speakCharacter } from "@/features/messages/speakCharacter";
-import { MessageInputContainer } from "@/components/messageInputContainer";
 import { getChatResponseStream } from "@/features/chat/chat";
-import { Introduction } from "@/components/introduction";
-import { LoadingProgress } from "@/components/loadingProgress";
-import { Menu } from "@/components/menu";
-import { EmbeddedWebcam } from "@/components/embeddedWebcam";
 import { config } from '@/utils/config';
 
  const m_plus_2 = M_PLUS_2({
@@ -37,6 +40,35 @@ export default function Home() {
   const [assistantMessage, setAssistantMessage] = useState("");
   const [showContent, setShowContent] = useState(false);
 
+
+  const [showSettings, setShowSettings] = useState(false);
+  const [showChatLog, setShowChatLog] = useState(false);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleClickOpenVrmFile = useCallback(() => {
+    fileInputRef.current?.click();
+  }, []);
+
+  const handleChangeVrmFile = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const files = event.target.files;
+      if (!files) return;
+
+      const file = files[0];
+      if (!file) return;
+
+      const file_type = file.name.split(".").pop();
+
+      if (file_type === "vrm") {
+        const blob = new Blob([file], { type: "application/octet-stream" });
+        const url = window.URL.createObjectURL(blob);
+        viewer.loadVrm(url);
+      }
+
+      event.target.value = "";
+    },
+    [viewer]
+  );
 
   useEffect(() => {
     document.body.style.backgroundImage = `url(${config("bg_url")})`;
@@ -217,11 +249,53 @@ export default function Home() {
         isChatProcessing={chatProcessing}
         onChatProcessStart={handleSendChat}
       />
-      <Menu
-        chatLog={chatLog}
-        assistantMessage={assistantMessage}
-        onChangeChatLog={handleChangeChatLog}
-        onClickResetChatLog={() => setChatLog([])}
+
+      <div className="absolute z-10 m-24">
+        <div className="grid grid-flow-col gap-[8px]">
+          <IconButton
+            iconName="24/Menu"
+            label="Settings"
+            isProcessing={false}
+            className="bg-secondary hover:bg-secondary-hover active:bg-secondary-active"
+            onClick={() => setShowSettings(true)}
+          ></IconButton>
+          {showChatLog ? (
+            <IconButton
+              iconName="24/CommentOutline"
+              label="Conversation Log"
+              isProcessing={false}
+              onClick={() => setShowChatLog(false)}
+            />
+          ) : (
+            <IconButton
+              iconName="24/CommentFill"
+              label="Conversation Log"
+              isProcessing={false}
+              disabled={chatLog.length <= 0}
+              onClick={() => setShowChatLog(true)}
+            />
+          )}
+        </div>
+      </div>
+      {showChatLog && <ChatLog messages={chatLog} />}
+      {showSettings && (
+        <Settings
+          chatLog={chatLog}
+          onClickClose={() => setShowSettings(false)}
+          onChangeChatLog={handleChangeChatLog}
+          onClickOpenVrmFile={handleClickOpenVrmFile}
+          onClickResetChatLog={() => setChatLog([])}
+        />
+      )}
+      {!showChatLog && assistantMessage && (
+        <AssistantText message={assistantMessage} />
+      )}
+      <input
+        type="file"
+        className="hidden"
+        accept=".vrm"
+        ref={fileInputRef}
+        onChange={handleChangeVrmFile}
       />
     </div>
   );
