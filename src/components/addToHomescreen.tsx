534a5b3de2847d255b0fe68d0fad1c778bdbfb0e
Kasumi Null <kasumi.null@yandex.com>
2023-11-06 04:09:39 +0800

    show add to homescreen above all

--- src/components/addToHomescreen.tsx
+++ src/components/addToHomescreen.tsx
@@ -29,7 +29,7 @@ export function AddToHomescreen() {
       {/* Global notification live region, render this permanently at the end of the document */}
       <div
         aria-live="assertive"
-        className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+        className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6 z-50"
         onClick={hide}
       >
         <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
