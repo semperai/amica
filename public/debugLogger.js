c45248eea36e75cbcf2127076eeb9e41e2a4ba44
Kasumi Null <kasumi.null@yandex.com>
2023-11-06 08:32:13 +0800

    improve debug logger

--- public/debugLogger.js
+++ public/debugLogger.js
@@ -2,39 +2,42 @@ if (typeof window !== "undefined") {
   if(! window.error_handler_installed) {
     window.error_handler_logs = [];
 
-    const cons = ((old) => ({
-      log: function() {
-        window.error_handler_logs.push({ type: 'log', ts: +new Date, arguments });
-        old.log.apply(null, arguments);
-      },
-      info: function() {
-        window.error_handler_logs.push({ type: 'info', ts: +new Date, arguments });
-        old.info.apply(null, arguments);
-      },
-      warn: function() {
-        window.error_handler_logs.push({ type: 'warn', ts: +new Date, arguments });
-        old.warn.apply(null, arguments);
-      },
-      error: function() {
-        window.error_handler_logs.push({ type: 'error', ts: +new Date, arguments });
-        old.error.apply(null, arguments);
-      },
-      time: function() {
-        old.time.apply(null, arguments);
-      },
-      timeEnd: function() {
-        old.timeEnd.apply(null, arguments);
-      },
+    const handler = ((old) => ({
+      get: (_, name) => {
+        function passf() {
+          old[name].apply(null, arguments);
+        }
+
+        function logf() {
+          window.error_handler_logs.push({
+            type: name,
+            ts: +new Date,
+            arguments,
+          });
+          passf.apply(null, arguments);
+        }
+
+        switch (name) {
+          case 'log':
+          case 'debug':
+          case 'info':
+          case 'warn':
+          case 'error':
+            return logf;
+          default:
+            return passf;
+        }
+      }
     }))(window.console);
-    window.console = cons;
+    window.console = new Proxy({}, handler);
 
     window.addEventListener("error", (e) => {
-      console.error("Error occurred: " + e.error.message);
+      console.error(`Error occurred: ${e.error.message} ${e.error.stack}`);
       return false;
     });
 
     window.addEventListener("unhandledrejection", (e) => {
-      console.error("Unhandled rejection occurred: " + e.reason.message);
+      console.error(`Unhandled rejection: ${e.message}`);
       return false;
     });
 
