323fd940f9faf051eba196225b0d1d91972ec47f
Kasumi Null <kasumi.null@yandex.com>
2023-11-13 09:52:13 +0800

    add vision settings

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -30,6 +30,8 @@ import {
   Cog6ToothIcon,
   PencilSquareIcon,
   PencilIcon,
+  EyeDropperIcon,
+  EyeIcon,
 } from '@heroicons/react/24/outline';
 
 import { getWindowAI } from "window.ai";
@@ -72,6 +74,11 @@ const sttEngines = [
   {key: "whisper_openai",  label: "Whisper (OpenAI)"},
 ];
 
+const visionEngines = [
+  {key: "none",       label: "None"},
+  {key: "llamacpp",   label: "LLama.cpp"},
+];
+
 function thumbPrefix(path: string) {
   const a = path.split("/");
   a[a.length - 1] = "thumb-" + a[a.length - 1];
@@ -107,6 +114,7 @@ function getIconFromPage(page: string): JSX.Element {
     case 'chatbot':             return <ChatBubbleOvalLeftEllipsisIcon className="h-5 w-5 flex-none text-gray-800" aria-hidden="true" />;
     case 'tts':                 return <MusicalNoteIcon className="h-5 w-5 flex-none text-gray-800" aria-hidden="true" />;
     case 'stt':                 return <PencilIcon className="h-5 w-5 flex-none text-gray-800" aria-hidden="true" />;
+    case 'vision':              return <EyeIcon className="h-5 w-5 flex-none text-gray-800" aria-hidden="true" />;
     case 'reset_settings':      return <PowerIcon className="h-5 w-5 flex-none text-red-500" aria-hidden="true" />;
     case 'community':           return <RocketLaunchIcon className="h-5 w-5 flex-none text-gray-800" aria-hidden="true" />;
 
@@ -128,6 +136,10 @@ function getIconFromPage(page: string): JSX.Element {
 
     case 'stt_backend':         return <PencilSquareIcon className="h-5 w-5 flex-none text-gray-800" aria-hidden="true" />;
     case 'whisper_openai_settings':  return <AdjustmentsHorizontalIcon className="h-5 w-5 flex-none text-gray-800" aria-hidden="true" />;
+
+    case 'vision_backend':           return <EyeDropperIcon className="h-5 w-5 flex-none text-gray-800" aria-hidden="true" />;
+    case 'vision_llamacpp_settings': return <AdjustmentsHorizontalIcon className="h-5 w-5 flex-none text-gray-800" aria-hidden="true" />;
+    case 'vision_system_prompt':     return <DocumentTextIcon className="h-5 w-5 flex-none text-gray-800" aria-hidden="true" />;
   }
 
   return <></>;
@@ -139,6 +151,7 @@ function getLabelFromPage(page: string): string {
     case 'chatbot':             return 'ChatBot';
     case 'tts':                 return 'Text-to-Speech';
     case 'stt':                 return 'Speech-to-text';
+    case 'vision':              return 'Vision';
     case 'reset_settings':      return 'Reset Settings';
     case 'community':           return 'Community';
 
@@ -157,6 +170,10 @@ function getLabelFromPage(page: string): string {
     case 'speecht5_settings':   return 'SpeechT5';
     case 'coqui_settings':      return 'Coqui';
     case 'openai_tts_settings': return 'OpenAI';
+
+    case 'vision_backend':           return 'Vision Backend';
+    case 'vision_llamacpp_settings': return 'LLama.cpp';
+    case 'vision_system_prompt':     return 'System Prompt';
 
     case 'stt_backend':         return 'STT Backend';
     case 'whisper_openai_settings': return "Whisper (OpenAI)";
@@ -1016,6 +1033,124 @@ function WhisperOpenAISettings({
   );
 }
 
+
+function VisionBackendPage({
+  visionBackend,
+  setVisionBackend,
+  setSettingsUpdated,
+  setPage,
+  breadcrumbs,
+  setBreadcrumbs,
+}: {
+  visionBackend: string;
+  setVisionBackend: (backend: string) => void;
+  setSettingsUpdated: (updated: boolean) => void;
+  setPage: (page: string) => void;
+  breadcrumbs: Link[];
+  setBreadcrumbs: (breadcrumbs: Link[]) => void;
+}) {
+  return basicPage(
+    "Vision Backend",
+    "Select the Vision backend to use",
+    <ul role="list" className="divide-y divide-gray-100 max-w-xs">
+      <li className="py-4">
+        <FormRow label="Vision Backend">
+          <select
+            className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            value={visionBackend}
+            onChange={(event: React.ChangeEvent<any>) => {
+              setVisionBackend(event.target.value);
+              updateConfig("vision_backend", event.target.value);
+              setSettingsUpdated(true);
+            }}
+          >
+            {visionEngines.map((engine) => (
+              <option key={engine.key} value={engine.key}>{engine.label}</option>
+            ))}
+          </select>
+        </FormRow>
+      </li>
+      { visionBackend === 'llamacpp' && (
+        <li className="py-4">
+          <FormRow label="Configure LLama.cpp">
+            <button
+              type="button"
+              className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={() => {
+                setPage('vision_llamacpp_settings');
+                setBreadcrumbs(breadcrumbs.concat([getLinkFromPage('vision_llamacpp_settings')]));
+              }}
+            >
+              Click here to configure LLama.cpp
+            </button>
+          </FormRow>
+        </li>
+      )}
+    </ul>
+  );
+}
+
+function VisionLlamaCppSettingsPage({
+  visionLlamaCppUrl,
+  setVisionLlamaCppUrl,
+  setSettingsUpdated,
+}: {
+  visionLlamaCppUrl: string;
+  setVisionLlamaCppUrl: (url: string) => void;
+  setSettingsUpdated: (updated: boolean) => void;
+}) {
+  return basicPage(
+    "LLama.cpp Settings",
+    <>LLama.cpp is a free and open source chatbot backend. You should build the server from source and run it on your own computer. You can get the source code from <a href="https://github.com/ggerganov/llama.cpp">GitHub</a></>,
+    <ul role="list" className="divide-y divide-gray-100 max-w-xs">
+      <li className="py-4">
+        <FormRow label="API URL">
+          <TextInput
+            value={visionLlamaCppUrl}
+            onChange={(event: React.ChangeEvent<any>) => {
+              setVisionLlamaCppUrl(event.target.value);
+              updateConfig("vision_llamacpp_url", event.target.value);
+              setSettingsUpdated(true);
+            }}
+          />
+        </FormRow>
+      </li>
+    </ul>
+  );
+}
+
+function VisionSystemPromptPage({
+  visionSystemPrompt,
+  setVisionSystemPrompt,
+  setSettingsUpdated,
+}: {
+  visionSystemPrompt: string;
+  setVisionSystemPrompt: (prompt: string) => void;
+  setSettingsUpdated: (updated: boolean) => void;
+}) {
+  return basicPage(
+    "Vision System Prompt",
+    "Configure the vision system prompt. This is the prompt that is used to generate the image descriptions.",
+    <ul role="list" className="divide-y divide-gray-100 max-w-xs">
+      <li className="py-4">
+        <FormRow label="Vision System Prompt">
+          <textarea
+            value={visionSystemPrompt}
+            rows={8}
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            onChange={(event: React.ChangeEvent<any>) => {
+              event.preventDefault();
+              setVisionSystemPrompt(event.target.value);
+              updateConfig("vision_system_prompt", event.target.value);
+              setSettingsUpdated(true);
+            }}></textarea>
+        </FormRow>
+      </li>
+    </ul>
+  );
+}
+
+
 function SystemPromptPage({
   systemPrompt,
   setSystemPrompt,
@@ -1176,6 +1311,10 @@ export const Settings = ({
   const [openAITTSModel, setOpenAITTSModel] = useState(config("openai_tts_model"));
   const [openAITTSVoice, setOpenAITTSVoice] = useState(config("openai_tts_voice"));
 
+  const [visionBackend, setVisionBackend] = useState(config("vision_backend"));
+  const [visionLlamaCppUrl, setVisionLlamaCppUrl] = useState(config("vision_llamacpp_url"));
+  const [visionSystemPrompt, setVisionSystemPrompt] = useState(config("vision_system_prompt"));
+
   const [bgUrl, setBgUrl] = useState(config("bg_url"));
   const [vrmUrl, setVrmUrl] = useState(config("vrm_url"));
   const [youtubeVideoID, setYoutubeVideoID] = useState(config("youtube_videoid"));
@@ -1273,6 +1412,7 @@ export const Settings = ({
     speechT5SpeakerEmbeddingsUrl,
     coquiApiKey, coquiVoiceId,
     openAITTSApiKey, openAITTSUrl, openAITTSModel, openAITTSVoice,
+    visionBackend, visionLlamaCppUrl, visionSystemPrompt,
     bgUrl, vrmUrl, youtubeVideoID, animationUrl,
     sttBackend,
     whisperOpenAIApiKey, whisperOpenAIModel, whisperOpenAIUrl,
@@ -1371,7 +1511,7 @@ export const Settings = ({
 
             {page === 'main_menu' && (
               <MenuPage
-                keys={["appearance", "chatbot", "tts", "stt", "reset_settings", "community"]}
+                keys={["appearance", "chatbot", "tts", "stt", "vision", "reset_settings", "community"]}
                 menuClick={handleMenuClick} />
             )}
 
@@ -1396,6 +1536,12 @@ export const Settings = ({
             {page === 'stt' && (
               <MenuPage
                 keys={["stt_backend", "whisper_openai_settings"]}
+                menuClick={handleMenuClick} />
+            )}
+
+            {page === 'vision' && (
+              <MenuPage
+                keys={["vision_backend", "vision_llamacpp_settings", "vision_system_prompt"]}
                 menuClick={handleMenuClick} />
             )}
 
@@ -1546,6 +1692,33 @@ export const Settings = ({
                 setWhisperOpenAIApiKey={setWhisperOpenAIApiKey}
                 whisperOpenAIModel={whisperOpenAIModel}
                 setWhisperOpenAIModel={setWhisperOpenAIModel}
+                setSettingsUpdated={setSettingsUpdated}
+                />
+            )}
+
+           {page === 'vision_backend' && (
+              <VisionBackendPage
+                visionBackend={visionBackend}
+                setVisionBackend={setVisionBackend}
+                setSettingsUpdated={setSettingsUpdated}
+                setPage={setPage}
+                breadcrumbs={breadcrumbs}
+                setBreadcrumbs={setBreadcrumbs}
+                />
+            )}
+
+            {page === 'vision_llamacpp_settings' && (
+              <VisionLlamaCppSettingsPage
+                visionLlamaCppUrl={visionLlamaCppUrl}
+                setVisionLlamaCppUrl={setVisionLlamaCppUrl}
+                setSettingsUpdated={setSettingsUpdated}
+                />
+            )}
+
+            {page === 'vision_system_prompt' && (
+              <VisionSystemPromptPage
+                visionSystemPrompt={visionSystemPrompt}
+                setVisionSystemPrompt={setVisionSystemPrompt}
                 setSettingsUpdated={setSettingsUpdated}
                 />
             )}
