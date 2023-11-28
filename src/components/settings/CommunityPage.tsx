88216906092fdebcbde795a285dd084f2e622755
Kasumi Null <kasumi.null@yandex.com>
2023-11-29 02:42:01 +0800

    add docs link to community

--- src/components/settings/CommunityPage.tsx
+++ src/components/settings/CommunityPage.tsx
@@ -5,7 +5,7 @@ export function CommunityPage() {
   return (
     <BasicPage
       title="Community"
-      description="Join the community"
+      description="Join the Amica community to connect with others exploring this technology."
     >
       <ul role="list" className="divide-y divide-gray-100 max-w-xs">
         <li className="py-4">
@@ -28,6 +28,15 @@ export function CommunityPage() {
         </li>
         <li className="py-4">
           <GitHubLink />
+        </li>
+        <li className="py-4">
+          <a
+            href="https://docs.heyamica.com"
+            target="_blank"
+            className="rounded bg-emerald-600 px-2 py-1 text-lg font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
+            >
+            Documentation
+          </a>
         </li>
       </ul>
     </BasicPage>
