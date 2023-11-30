59ff25559c9be49884375ec570b4fd3c101dd23c
Kasumi Null <kasumi.null@yandex.com>
2023-11-30 08:51:47 +0800

    begin upload for character share

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -1,7 +1,29 @@
+import { createHash } from 'crypto';
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { config, updateConfig } from '@/utils/config';
+import { supabase } from '@/utils/supabase';
+import { FilePond, registerPlugin } from 'react-filepond';
 
+
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
+registerPlugin(
+  FilePondPluginImagePreview,
+  FilePondPluginFileValidateType,
+);
+
+async function hashFile(file: File): Promise<string> {
+  const buffer = await file.arrayBuffer();
+  const hashValue = createHash('sha256')
+    .update(Buffer.from(buffer))
+    .digest('hex');
+  return hashValue;
+}
 
 export default function Share() {
   const [systemPrompt, setSystemPrompt] = useState('');
@@ -10,6 +32,12 @@ export default function Share() {
   const [youtubeVideoId, setYoutubeVideoId] = useState('');
   const [vrmUrl, setVrmUrl] = useState('');
   const [animationUrl, setAnimationUrl] = useState('');
+  const [voiceUrl, setVoiceUrl] = useState('');
+
+  const [bgFiles, setBgFiles] = useState([]);
+  const [vrmFiles, setVrmFiles] = useState([]);
+  const [animationFiles, setAnimationFiles] = useState([]);
+  const [voiceFiles, setVoiceFiles] = useState([]);
 
   useEffect(() => {
     setSystemPrompt(config('system_prompt'));
@@ -78,6 +106,39 @@ export default function Share() {
               setBgUrl(e.target.value);
             }}
           />
+          <FilePond
+            files={bgFiles}
+            // this is done to remove type error
+            // filepond is not typed properly
+            onupdatefiles={(files: any) => {
+              setBgFiles(files);
+            }}
+            server={`${process.env.NEXT_PUBLIC_AMICA_API_URL}/api/upload?type=bgimg`}
+            name="file"
+            labelIdle='.png & .jpg files only<br />click or drag & drop'
+            acceptedFileTypes={['image/png', 'image/jpeg']}
+            onremovefile={(err, file) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+
+              setBgUrl('');
+            }}
+            onprocessfile={(err, file) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+
+              async function handleFile(file: File) {
+                const hashValue = await hashFile(file);
+                setBgUrl(`${process.env.NEXT_PUBLIC_AMICA_STORAGE_URL}/${hashValue}`);
+              }
+
+              handleFile(file.file as File);
+            }}
+          />
         </div>
       </div>
 
@@ -110,6 +171,39 @@ export default function Share() {
               setVrmUrl(e.target.value);
             }}
           />
+          <FilePond
+            files={vrmFiles}
+            // this is done to remove type error
+            // filepond is not typed properly
+            onupdatefiles={(files: any) => {
+              setVrmFiles(files);
+            }}
+            server={`${process.env.NEXT_PUBLIC_AMICA_API_URL}/api/upload?type=vrm`}
+            name="file"
+            labelIdle='.vrm files only<br />click or drag & drop'
+            acceptedFileTypes={['model/gltf-binary']}
+            onremovefile={(err, file) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+
+              setVrmUrl('');
+            }}
+            onprocessfile={(err, file) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+
+              async function handleFile(file: File) {
+                const hashValue = await hashFile(file);
+                setVrmUrl(`${process.env.NEXT_PUBLIC_AMICA_STORAGE_URL}/${hashValue}`);
+              }
+
+              handleFile(file.file as File);
+            }}
+          />
         </div>
       </div>
 
@@ -124,6 +218,89 @@ export default function Share() {
             defaultValue={animationUrl}
             onChange={(e) => {
               setAnimationUrl(e.target.value);
+            }}
+          />
+          <FilePond
+            files={animationFiles}
+            // this is done to remove type error
+            // filepond is not typed properly
+            onupdatefiles={(files: any) => {
+              setAnimationFiles(files);
+            }}
+            // TODO read this url from env
+            server={`${process.env.NEXT_PUBLIC_AMICA_API_URL}/api/upload?type=anim`}
+            name="file"
+            labelIdle='.vrm files only<br />click or drag & drop'
+            acceptedFileTypes={['model/gltf-binary']}
+            onremovefile={(err, file) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+
+              setAnimationUrl('');
+            }}
+            onprocessfile={(err, file) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+
+              async function handleFile(file: File) {
+                const hashValue = await hashFile(file);
+                setAnimationUrl(`${process.env.NEXT_PUBLIC_AMICA_STORAGE_URL}/${hashValue}`);
+              }
+
+              handleFile(file.file as File);
+            }}
+          />
+        </div>
+      </div>
+
+      <div className="sm:col-span-3 max-w-xs rounded-xl mt-4">
+        <label className="block text-sm font-medium leading-6 text-gray-900">
+          Voice Url
+        </label>
+        <div className="mt-2">
+          <input
+            type="text"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            defaultValue={voiceUrl}
+            onChange={(e) => {
+              setVoiceUrl(e.target.value);
+            }}
+          />
+          <FilePond
+            files={voiceFiles}
+            // this is done to remove type error
+            // filepond is not typed properly
+            onupdatefiles={(files: any) => {
+              setVoiceFiles(files);
+            }}
+            server={`${process.env.NEXT_PUBLIC_AMICA_API_URL}/api/upload?type=voice`}
+            name="file"
+            labelIdle='.wav & .mp3 files only<br />click or drag & drop'
+            acceptedFileTypes={['audio/wav', 'audio/mpeg']}
+            onremovefile={(err, file) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+
+              setVoiceUrl('');
+            }}
+            onprocessfile={(err, file) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+
+              async function handleFile(file: File) {
+                const hashValue = await hashFile(file);
+                setVoiceUrl(`${process.env.NEXT_PUBLIC_AMICA_STORAGE_URL}/${hashValue}`);
+              }
+
+              handleFile(file.file as File);
             }}
           />
         </div>
---
+++ src/utils/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+);
