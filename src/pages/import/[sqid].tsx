0a26b1d9617fc4705d6891d883fb321d05e07144
Kasumi Null <kasumi.null@yandex.com>
2023-12-07 05:16:45 +0800

    add space in error

--- src/pages/import/[sqid].tsx
+++ src/pages/import/[sqid].tsx
@@ -127,6 +127,7 @@ export default function Import() {
           <div>
             <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
               <strong className="font-bold">{t("Error!")}</strong>
+              {' '}
               <span className="block sm:inline">{t("Something went wrong.")}</span>
             </div>
             <p className="mt-8">
