15a54521a6cad0c813ed28084c208da9c2ef6499
Kasumi Null <kasumi.null@yandex.com>
2023-12-12 05:21:14 +0800

    show voice url in import

--- src/pages/import/[sqid].tsx
+++ src/pages/import/[sqid].tsx
@@ -308,6 +308,22 @@ export default function Import() {
               </div>
             </div>
           )}
+
+          {voiceUrl && voiceUrl != defaultConfig('voice_url') && (
+            <div className="sm:col-span-3 max-w-md rounded-xl mt-4">
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                {t("Voice Url")}
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={voiceUrl}
+                  disabled={true}
+                />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
