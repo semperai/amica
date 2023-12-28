42148a91706374dad0e3165a1f8eb5c9a3fe3320
Riceball LEE <snowyu.lee@gmail.com>
2023-12-28 10:59:33 +0800

    test: more test added

--- src/backends/custom-backend.test.ts
+++ src/backends/custom-backend.test.ts
@@ -2,14 +2,30 @@ import { CustomBackend } from "./custom-backend"
 
 describe('CustomBackend', () => {
   class MyTestBackend extends CustomBackend {}
-  it('should register backend', ()=>{
+
+  beforeAll(()=>{
     CustomBackend.registerBackend(MyTestBackend)
+  })
+  afterAll(()=>{
+    expect(CustomBackend.unregister(MyTestBackend)).toBeTruthy()
+  })
+
+  function testMyTest(myTest: MyTestBackend) {
+    expect(myTest).toBeInstanceOf(MyTestBackend)
+    expect(myTest.name).toBe('MyTest')
+    expect(myTest).toHaveProperty('enabled', true)
+  }
+
+  it('should register backend', ()=>{
     expect(CustomBackend.registeredClass('MyTest')).toBe(MyTestBackend)
     expect(MyTestBackend.enabled).toBeTruthy()
   })
+  it('should get BackendSchema', ()=>{
+    const expetedResult = {name: {required: true, type: 'string'}, enabled: {type: 'boolean', value: true}}
+    expect(CustomBackend.getProperties()).toMatchObject(expetedResult)
+  })
   it('should create new backend', ()=>{
-    const myTest = new MyTestBackend
-    expect(myTest.name).toBe('MyTest')
-    expect(myTest).toHaveProperty('enabled', true)
+    testMyTest(new MyTestBackend)
+    testMyTest(new CustomBackend(null, 'MyTest'))
   })
 })
