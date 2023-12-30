a0dbf696fd7b6b595dbf418cb7f6461164235bd5
Riceball LEE <snowyu.lee@gmail.com>
2023-12-30 10:40:58 +0800

    fix: set VisionBackend temperature default value to 0

--- src/backends/vision-backend.ts
+++ src/backends/vision-backend.ts
@@ -1,5 +1,8 @@
+import cloneDeep from 'lodash/cloneDeep'
+
 import { Backend } from './backend';
 import { BotBackend, MessageParams } from './bot-backend';
+import { BotProps, BotPropsSchema } from './bot-options';
 
 export interface VisionMessageParams extends MessageParams {
   imageData: string|string[]
@@ -8,6 +11,11 @@ export interface VisionMessageParams extends MessageParams {
 export class VisionBackend extends BotBackend {
   async getResponse(options: VisionMessageParams): Promise<string> { return '' }
 }
+
+const VisionPropsSchema = cloneDeep(BotPropsSchema)
+VisionPropsSchema.temperature.value = 0
+
+VisionBackend.defineProperties(VisionBackend, VisionPropsSchema)
 
 Backend.register(VisionBackend)
 
