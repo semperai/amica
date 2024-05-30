9fb2f2da3dc5dcc5ec63f93cb4ea838263e69ee8
flukexp <flukelolza@gmail.com>
2024-05-30 18:43:27 +0700

    added image uploader

--- src/components/embeddedWebcam.tsx
+++ src/components/embeddedWebcam.tsx
@@ -16,21 +16,54 @@ export function EmbeddedWebcam({
   const [facingMode, setFacingMode] = useState<"user" | "environment">("environment");
   const [cameraDisabled, setCameraDisabled] = useState(false);
   const [imageData, setImageData] = useState("");
+  const [imageMode, setImageMode] = useState<"webcam" | "uploader">("webcam");
+  const imgRef = useRef<HTMLImageElement>(null);
 
   useKeyboardShortcut("Escape", () => {
     setWebcamEnabled(false);
   });
 
   useEffect(() => {
-    (async () => {
-      if (imageData !== "") {
-        const fixed = imageData.replace("data:image/jpeg;base64,", "");
-        await bot.getVisionResponse(fixed);
+    const processImageData = async () => {
+      if (!imageData) return;
+
+      let processedData = imageData;
+      if (imageMode !== "webcam") {
+        processedData = await drawImageOnCanvas(imageData);
+      } else {
+        processedData = imageData.replace(`data:image/jpeg;base64,`, "");
       }
 
+      await bot.getVisionResponse(processedData);
       setCameraDisabled(false);
-    })();
-  }, [imageData]);
+    };
+
+    const drawImageOnCanvas = (src: string): Promise<string> => {
+      return new Promise((resolve, reject) => {
+        const canvas = document.createElement('canvas');
+        canvas.width = imgRef.current!.width;
+        canvas.height = imgRef.current!.height;
+        const context = canvas.getContext('2d');
+        const img = new Image();
+
+        if (!context) {
+          reject(new Error("Canvas context is not available"));
+          return;
+        }
+
+        img.onload = () => {
+          context.drawImage(img, 0, 0, imgRef.current!.width, imgRef.current!.height);
+          const dataUrl = canvas.toDataURL('image/jpeg');
+          resolve(dataUrl.replace('data:image/jpeg;base64,', ''));
+        };
+
+        img.onerror = reject;
+        img.src = src;
+      });
+    };
+
+    processImageData();
+  }, [imageData, imageMode, bot]);
 
   const capture = useCallback(
     () => {
@@ -42,17 +75,44 @@ export function EmbeddedWebcam({
       if (imageSrc) {
         setCameraDisabled(true);
         setImageData(imageSrc);
+        setImageMode('webcam');
       }
     },
     [webcamRef]
   );
+
+  const imgFileInputRef = useRef<HTMLInputElement>(null);
+  const handleClickOpenImgFile = useCallback(() => {
+    imgFileInputRef.current?.click();
+  }, []);
+
+  const handleChangeImgFile = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const files = event.target.files;
+      if (!files) return;
+
+      const file = files?.[0];
+      if (!file) return;
+
+      if (!file.type.match('image.*')) return;
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        let imageSrc = reader.result as string;
+        setCameraDisabled(true);
+        setImageMode('uploader');
+        setImageData(imageSrc);
+      };
+      reader.readAsDataURL(file);
+
+    }, []);
 
 
   return (
     <div className="fixed right-[calc(320px)] top-0 z-[11]">
       <div className="fixed">
         <>
-          { ! cameraDisabled && (
+          {!cameraDisabled && (
             <Webcam
               ref={webcamRef}
               audio={false}
@@ -68,8 +128,9 @@ export function EmbeddedWebcam({
               )}
             />
           )}
-          { cameraDisabled && (
+          {cameraDisabled && (
             <img
+              ref={imgRef}
               src={imageData}
               alt="Captured image"
               width={320}
@@ -78,9 +139,16 @@ export function EmbeddedWebcam({
                 "rounded-bl-none rounded-br-none rounded-lg bg-black",
                 cameraDisabled && "animate-pulse",
               )}
-              />
+            />
           )}
-          <div className="p-1 shadow-md flex flex-auto justify-center bg-gray-50 rounded-tl-none rounded-tr-none rounded-full">
+          <div className="p-1 shadow-md flex flex-auto justify-evenly bg-gray-50 rounded-tl-none rounded-tr-none rounded-full">
+            <IconButton
+              iconName="24/UploadAlt"
+              isProcessing={false}
+              className="bg-secondary hover:bg-secondary-hover active:bg-secondary-active"
+              onClick={handleClickOpenImgFile}
+              disabled={cameraDisabled}
+            />
             <IconButton
               iconName="24/Shutter"
               isProcessing={false}
@@ -89,21 +157,29 @@ export function EmbeddedWebcam({
               disabled={cameraDisabled}
             />
 
-            <button className="ml-8 px-1.5 rounded-lg text-sm p-1 text-center inline-flex items-center">
-            <ArrowPathIcon
-              className="w-5 h-5 text-gray-700 focus:animate-spin"
-              onClick={() => {
-                if (facingMode === 'user') {
-                  setFacingMode('environment');
-                } else if (facingMode === 'environment') {
-                  setFacingMode('user');
-                }
-              }}
-            />
+            <button className="pr-2 rounded-lg text-sm p-1 text-center inline-flex items-center">
+              <ArrowPathIcon
+                className="w-5 h-5 text-gray-700 focus:animate-spin"
+                onClick={() => {
+                  if (facingMode === 'user') {
+                    setFacingMode('environment');
+                  } else if (facingMode === 'environment') {
+                    setFacingMode('user');
+                  }
+                }}
+              />
             </button>
           </div>
         </>
       </div>
+      <input
+        type="file"
+        className="hidden"
+        accept="image/*"
+        ref={imgFileInputRef}
+        onChange={handleChangeImgFile}
+      />
     </div>
-  )
+
+  );
 }
