60bc34e56eb96e64cbcf2624f974ed5737eabd0d
Kasumi Null <kasumi.null@yandex.com>
2024-09-27 11:44:03 +0800

    fix indentation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -352,12 +352,12 @@ export default function Home() {
             </div>
 
             <div className="flex flex-row items-center space-x-2">
-                <CodeBracketSquareIcon
-                  className="h-7 w-7 text-white opacity-50 hover:opacity-100 active:opacity-100 hover:cursor-pointer"
-                  aria-hidden="true"
-                  onClick={() => setShowDebug(true)}
-                />
-                <span className="text-white hidden">Debug</span> 
+              <CodeBracketSquareIcon
+                className="h-7 w-7 text-white opacity-50 hover:opacity-100 active:opacity-100 hover:cursor-pointer"
+                aria-hidden="true"
+                onClick={() => setShowDebug(true)}
+              />
+              <span className="text-white hidden">Debug</span>
             </div>
 
             <div className="flex flex-row items-center space-x-2">
