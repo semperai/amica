2ff240da6db98abcd839de47cc98e8abba942c76
Kasumi Null <kasumi.null@yandex.com>
2024-09-28 14:57:18 +0800

    disable autocomplete on messageinput

--- src/components/messageInput.tsx
+++ src/components/messageInput.tsx
@@ -240,7 +240,9 @@ export default function MessageInput({
             disabled={false}
 
             className="disabled block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-gray-400 sm:text-sm sm:leading-6"
-            value={userMessage}></input>
+            value={userMessage}
+            autoComplete="off"
+          />
 
           <div className='flex flex-col justify-center items-center'>
             <IconButton
