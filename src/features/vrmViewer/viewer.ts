94ec4609e360f5bfe558c432bd2424377a7ddc9d
Kasumi Null <kasumi.null@yandex.com>
2024-09-29 18:01:15 +0800

    increase lighting as three or three-vrm update reduced it

--- src/features/vrmViewer/viewer.ts
+++ src/features/vrmViewer/viewer.ts
@@ -44,11 +44,11 @@ export class Viewer {
     this._scene = scene;
 
     // light
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
     directionalLight.position.set(1.0, 1.0, 1.0).normalize();
     scene.add(directionalLight);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2.);
     scene.add(ambientLight);
 
     // animate
