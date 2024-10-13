4dafca46187d5cd75eb3f1dd6443a70cb25373f2
Kasumi Null <kasumi.null@yandex.com>
2024-10-13 18:53:05 +0800

    add white floor

--- src/features/vrmViewer/viewer.ts
+++ src/features/vrmViewer/viewer.ts
@@ -78,6 +78,16 @@ export class Viewer {
     const ambientLight = new THREE.AmbientLight(0xffffff, 2.);
     scene.add(ambientLight);
 
+    const floorGeometry = new THREE.PlaneGeometry(100, 100);
+    const floorMaterial = new THREE.MeshBasicMaterial({
+      color: 0xFFFFFF,
+      side: THREE.DoubleSide,
+    });
+    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.rotation.x = Math.PI / 2;
+    floor.receiveShadow = true;
+    scene.add(floor);
+
     // animate
     this._clock = new THREE.Clock();
     this._clock.start();
