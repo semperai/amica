2dae3ead384f79c4c6fc1d1affc57939bc21b0ef
Kasumi Null <kasumi.null@yandex.com>
2024-10-15 19:05:01 +0800

    remove shadows from room

--- src/features/vrmViewer/room.ts
+++ src/features/vrmViewer/room.ts
@@ -13,23 +13,7 @@ export class Room {
 
   public async loadRoom(url: string): Promise<void> {
     const loader = new GLTFLoader();
-
     const gltf = await loader.loadAsync(url);
-    gltf.scene.traverse(function (child) {
-      if ((child as THREE.Mesh).isMesh) {
-        const m = child as THREE.Mesh
-        m.receiveShadow = true
-        m.castShadow = true
-      }
-      if ((child as THREE.Light).isLight) {
-        const l = child as THREE.SpotLight
-        l.castShadow = true
-        l.shadow.bias = -0.003
-        l.shadow.mapSize.width = 2048
-        l.shadow.mapSize.height = 2048
-      }
-    });
-
     this.room = gltf.scene;
   }
 
