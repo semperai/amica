fe870abae7dfce91a891cc45b5aaf6b62245f3ac
Kasumi Null <kasumi.null@yandex.com>
2024-10-23 12:54:58 +0800

    disable sentry if not in prod

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,9 @@
 import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
+  if (process.env.NODE_ENV === "development") {
+    return;
+  }
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("../sentry.server.config");
   }
