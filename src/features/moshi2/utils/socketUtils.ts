c3c9986abe9ab8013dc76b05636604938fac50e8
flukexp <flukelolza@gmail.com>
2024-12-05 16:15:29 +0700

    fixed getBaseUrl

--- src/features/moshi2/utils/socketUtils.ts
+++ src/features/moshi2/utils/socketUtils.ts
@@ -1,7 +1,8 @@
 import { config } from "@/utils/config";
 
-export const getBaseURL = (): URL => {
-    const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-    const url = new URL(`${wsProtocol}://${config("moshi_url")}/ws`);
-    return url;
+export const getBaseURL = (): string => {
+    const currentURL = new URL(config("moshi_url"));
+    const hostname = currentURL.hostname;
+    const wsProtocol = currentURL.protocol === 'https:' ? 'wss:' : 'ws:';
+    return `${wsProtocol}//${hostname}/ws`; 
 };
