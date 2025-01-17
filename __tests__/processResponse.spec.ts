ea992bca3bce6c09b1dcf8bce6a626c22d13d2c2
flukexp <flukelolza@gmail.com>
2025-01-18 00:19:55 +0700

    fixed test suit

--- __tests__/processResponse.spec.ts
+++ __tests__/processResponse.spec.ts
@@ -8,6 +8,7 @@ describe("Process Response Tests", () => {
     const aiTextLog = "";
     const receivedMessage = "";
     const tag = "";
+    const rolePlay = "";
 
     let cbTriggered = false;
 
@@ -16,6 +17,7 @@ describe("Process Response Tests", () => {
       aiTextLog,
       receivedMessage,
       tag,
+      rolePlay,
       callback: (aiTalks: Screenplay[]) => {
         cbTriggered = true;
         expect(aiTalks).toEqual([]);
@@ -26,6 +28,7 @@ describe("Process Response Tests", () => {
     expect(proc.aiTextLog).toBe("");
     expect(proc.receivedMessage).toBe("");
     expect(proc.tag).toBe("");
+    expect(proc.rolePlay).toBe("");
     expect(proc.shouldBreak).toBe(false);
     expect(cbTriggered).toBe(false);
   });
