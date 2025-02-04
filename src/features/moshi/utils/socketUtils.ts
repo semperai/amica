48d56c86b4fc8a87e0130e18c76c4314d3d51f60
flukexp <flukelolza@gmail.com>
2025-02-04 20:59:04 +0700

    fixed ws port setup

--- src/features/moshi/utils/socketUtils.ts
+++ src/features/moshi/utils/socketUtils.ts
@@ -3,6 +3,7 @@ import { config } from "@/utils/config";
 export const getBaseURL = (): string => {
     const currentURL = new URL(config("moshi_url"));
     const hostname = currentURL.hostname;
+    const port = currentURL.port ? `:${currentURL.port}` : '';
     const wsProtocol = currentURL.protocol === 'https:' ? 'wss:' : 'ws:';
-    return `${wsProtocol}//${hostname}/ws`; 
+    return `${wsProtocol}//${hostname}${port}/ws`; 
 };
