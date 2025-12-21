612952e1a08945762dd4fc6289f110e6cc98b5f0
CodeShadower <12210333+CodeShadower@users.noreply.github.com>
2025-12-21 19:41:09 +0100

    Added French translation

--- src/i18n/langs.ts
+++ src/i18n/langs.ts
@@ -2,6 +2,7 @@ import en from "./locales/en/common.json";
 import zh from "./locales/zh/common.json";
 import de from "./locales/de/common.json";
 import ka from "./locales/ka/common.json";
+import fr from "./locales/fr/common.json";
 
 // const langs: any = Object.entries({en, zh}).reduce((arr, [key, value]) => {
 //     return {...arr, [key] : { translation: value} }
@@ -23,6 +24,10 @@ export const langs: any = {
   ka: {
     nativeName: 'ქართული',
     translation: ka,
+  },
+  fr: {
+    nativeName: 'Français',
+    translation: fr,
   },
 }
 
